cf31b6a76d19238c05cb5c74db772a2d64e05862
Jairo Antonio Melo Flórez <jairomelo@users.noreply.github.com>
2017-08-11 22:59:06 +0200

    link to item = _blank
    Prefiero que los enlaces se desplieguen en una ventana nueva para no tener que regresar siempre a la página principal.

--- items/browse.php
+++ items/browse.php
@@ -29,7 +29,7 @@ $sortLinks[__('Fecha del elemento')] = 'Dublin Core,Date';
 
 <?php foreach (loop('items') as $item): ?>
 <div class="item record">
-    <h2><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></h2>
+    <h2><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink', 'target'=>'_blank')); ?></h2>
     <div class="item-meta">
     <?php if (metadata('item', 'has files')): ?>
     <div class="item-img">
